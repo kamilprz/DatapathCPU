ce8ff1a2364f632b45aa68d5d8807dbc6834d7c4
kamilprz <36544756+kamilprz@users.noreply.github.com>
2019-02-13 14:51:21 +0000

    testbenches

---
+++ decoder_3to8_tb.vhd
@@ -0,0 +1,83 @@
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY decoder_3to8_tb IS
+END decoder_3to8_tb;
+ 
+ARCHITECTURE behavior OF decoder_3to8_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT decoder_3to8_tb
+    PORT(
+         A : IN  std_logic;
+         B : IN  std_logic;
+         C : IN  std_logic;
+         Q0 : OUT std_logic;
+         Q1 : OUT std_logic;
+         Q2 : OUT std_logic;
+         Q3 : OUT std_logic;
+         Q4 : OUT std_logic;
+         Q5 : OUT std_logic;
+         Q6 : OUT std_logic;
+         Q7 : OUT std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic := '0';
+   signal B : std_logic := '0';
+   signal C : std_logic := '0';
+
+
+ 	--Outputs
+   signal Q0 : std_logic := '0';
+   signal Q1 : std_logic := '0';
+   signal Q2 : std_logic := '0';
+   signal Q3 : std_logic := '0';
+   signal Q4 : std_logic := '0';
+   signal Q5 : std_logic := '0';
+   signal Q6 : std_logic := '0';
+   signal Q7 : std_logic := '0';
+
+   
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+--   constant Clk_period : time := 10 ns;
+ 
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+   uut: decoder_3to8_tb PORT MAP (
+          A => A,
+          B => B,
+          C => C,
+          Q0 => Q0,
+          Q1 => Q1,
+          Q2 => Q2,
+          Q3 => Q3,
+          Q4 => Q4,
+          Q5 => Q5,
+          Q6 => Q6,
+          Q7 => Q7
+        );
+
+   stim_proc: process
+   begin		
+    wait for 10 ns;
+    A <= '0';
+    B <= '0';
+    C <= '1';
+    wait for 10 ns;
+    
+   end process;
+
+END;
---
+++ mux_2to1_tb.vhd
@@ -0,0 +1,63 @@
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY mux_2to1_tb IS
+END mux_2to1_tb;
+ 
+ARCHITECTURE behavior OF mux_2to1_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT mux_2to1
+    PORT(
+         s : IN  std_logic;
+         A : IN  std_logic_vector(15 downto 0);
+         B : IN  std_logic_vector(15 downto 0);
+         Z : OUT std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal s : std_logic := '0';
+   signal A : std_logic_vector(15 downto 0) := (others => '0');
+   signal B : std_logic_vector(15 downto 0) := (others => '0');
+
+
+ 	--Outputs
+   signal Z : std_logic_vector(15 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+--   constant Clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: mux_2to1 PORT MAP (
+          s => s,
+          A => A,
+          B => B,
+          Z => Z
+        );
+
+   stim_proc: process
+   begin		
+        A <= "1111000011110000";
+		B <= "0000111100001111";
+      wait for 10 ns;	
+        s <= '0';
+
+      wait for 10 ns;	
+		s <= '1';
+ --     wait;
+   end process;
+
+END;
